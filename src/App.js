4fc5eda2d9f35b017f00be4e2f4c9703aadb1540
MHammad-374 <143274487+MHammad-374@users.noreply.github.com>
2024-02-06 19:17:14 +0500

    check

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css'
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
-import { apidata } from './api-Data/apidata';
 
 
 
